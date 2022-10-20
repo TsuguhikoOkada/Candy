4d9f3a85140fb11e540fec2b706a99ab80a8f955
TsuguhikoOkada <83790901+TsuguhikoOkada@users.noreply.github.com>
2022-10-20 22:08:39 +0900

    Update Bomb.cs

--- 2d_puzzle/Assets/Scripts/Bomb.cs
+++ 2d_puzzle/Assets/Scripts/Bomb.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
@@ -8,26 +8,10 @@ public class Bomb : MonoBehaviour
     void OnMouseDown()
     {
         Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 1f);
+        
         GetComponentInParent<BlockManager>().OnBlockClear(colliders.Length);
 
-        // Linqを使用したラムダ式で書いたコード↓
-
         colliders.Where(collider => Block.IsBlock(collider.gameObject)).ToList().ForEach(collider => Destroy(collider.gameObject));
-
-        // ここまで↑
-
-        // 修正前のコード↓
-
-        /*foreach (Collider2D collider in colliders)
-        {
-            if (Block.IsBlock(collider.gameObject))
-            {
-                Destroy(collider.gameObject);
-                
-            }
-        }*/
-
-        // ここまで↑
 
         Destroy(gameObject);
     }
